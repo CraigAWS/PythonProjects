ed6cbddb69baaec17753d10eb097eb22a3df1092
Craig Sinnott <157162317+CraigAWS@users.noreply.github.com>
2024-08-02 21:18:29 +0100

    Delete Python Projects directory

--- Python Projects/BandGEN.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to band name generator!")
-Drink = input("Please enter the name of your favorite drink\n")
-Movie = input("Please enter the name of your favorite movie\n")
-print("Your Band name could be " + Drink + " " + Movie + "!")
