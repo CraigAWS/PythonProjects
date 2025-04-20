209ff034ec07b01c0a00882294a455c45a80b4ad
craigsinnott@hotmail.co.uk <craigsinnott@hotmail.co.uk>
2025-04-20 21:42:24 +0100

    Logical Operators and Condition Expressions

---
+++ Beginner Projects/Condition-Expressions.py
@@ -0,0 +1,15 @@
+num = 5
+a = 6
+b = 7
+age = 1
+temperature = 20
+user_role = "stuff"
+
+# result = "EVEN" if num % 2 == 0 else "ODD"
+
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temperature > 20 else "COLD"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+print(access_level)
